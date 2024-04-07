062e823010e5917e86831ba5eec83ba52841ed9b
kpei4ni <mkoragin09@gmail.com>
2024-04-07 15:46:35 +0300

    remake previous task

--- Work/Work.cpp
+++ Work/Work.cpp
@@ -2,8 +2,7 @@
 #include <Windows.h>
 using namespace std;
 
-
-enum Bill{
+enum Bill {
     franko = 20,
     grushevskiy = 50,
     shevchenko = 100,
@@ -17,31 +16,15 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int answer;
+    string answer;
+
+    cout << "Яке прізвище вас цікавить? " << endl; cin >> answer;
+
     Bill legend;
 
-    cout << "Кого вам вивести? " << endl; cin >> answer;
+    (answer == "Українка") ? legend = ukrainka : (answer == "Сковорода") ? legend = skovoroda : legend = vernadskiy;
 
-    
-    switch (answer) {
-        case 20: legend = franko; break;
-        case 50: legend = grushevskiy; break;
-        case 100: legend = shevchenko; break;
-        case 200: legend = ukrainka; break;
-        case 500: legend = skovoroda; break;
-        case 1000: legend = vernadskiy; break;
-    }
-
-    cout << "На банкноті " << answer << " знаходиться ";
-
-    switch (legend) {
-        case franko:  cout << "Іван Франко" << endl; break;
-        case grushevskiy:  cout << "Михайло Грушевський" << endl; break;
-        case shevchenko:  cout << "Тарас Шевченко" << endl; break;
-        case ukrainka:  cout << "Леся Українка (Лариса Косач)" << endl; break;
-        case skovoroda:  cout << "Григорів Сковорода" << endl; break;
-        case vernadskiy:  cout << "Володимир Вернадський" << endl; break;
-    }
+    cout << "Банкнота з " << answer << " номіналом " << legend << endl;
 
 
 }

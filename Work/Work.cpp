2d2620869ce53c16fecbe9fbf90cce0dbc84748b
kpei4ni <mkoragin09@gmail.com>
2024-04-07 16:21:08 +0300

    random number

--- Work/Work.cpp
+++ Work/Work.cpp
@@ -3,8 +3,20 @@
 #include <ctime> 
 #include <CoreWindow.h> 
 #include <Windows.h>
-
 using namespace std;
+
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define BLUE "\033[0;34m"
+#define YELLOW "\033[1;33m"
+#define CYAN "\033[0;36m"
+#define ORANGE "\033[0;33m"
+#define PURPLE "\033[0;35m"
+#define WHITE "\033[1;37m"
+#define GREY "\033[1;30m"
+#define PINK "\033[1;35m"
+#define BLACK "\033[0;30m"
+
 
 enum Bill {
     
@@ -18,21 +30,29 @@ int main()
 
     srand(time(nullptr));
 
-    int num1 = 1 + rand() % 10;
+    int gess;
 
-    cout << num1 << endl;
+    int r_num = 1 + rand() % 12;
 
-    cout << "" << endl;
+    cout << "¬вед≥ть чисало в≥д 1 до 12: "; cin >> gess;
 
-    int num2 = -5 + rand() % 11;
+    (gess == r_num) ? cout << "¬и вгадали ваше число " << endl : cout << "“и лох!" << endl;
 
-    cout << num2 << endl;
+    switch (r_num) {
+        case 1: cout << RED << "1" << WHITE << endl; break;
+        case 2: cout << GREEN << "2" << WHITE << endl; break;
+        case 3: cout << BLUE << "3" << WHITE << endl; break;
+        case 4: cout << YELLOW << "4" << WHITE << endl; break;
+        case 5: cout << CYAN << "5" << WHITE << endl; break;
+        case 6: cout << ORANGE << "6" << WHITE << endl; break;
+        case 7: cout << PURPLE << "7" << WHITE << endl; break;
+        case 8: cout << WHITE << "8" << WHITE << endl; break;
+        case 9: cout << GREY << "9" << WHITE << endl; break;
+        case 10: cout << GREY << "10" << WHITE << endl; break;
+        case 11: cout << PINK << "11" << PINK << endl; break;
+        case 12: cout << BLACK << "12" << BLACK << endl; break;
+    }
 
-    cout << "" << endl;
-
-    int num3 = rand() % 2;
-
-    cout << num3 << endl;
 
 
 

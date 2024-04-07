5ff191227cc7c6b9fb0515c221599385cc88cd11
kpei4ni <mkoragin09@gmail.com>
2024-04-07 15:37:01 +0300

    add Output in console the name of legend thats place on a bill

--- Work/Work.cpp
+++ Work/Work.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
+
+
+enum Bill{
+    franko = 20,
+    grushevskiy = 50,
+    shevchenko = 100,
+    ukrainka = 200,
+    skovoroda = 500,
+    vernadskiy = 1000
+};
 
 int main()
 {
-    cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int answer;
+    Bill legend;
+
+    cout << "Кого вам вивести? " << endl; cin >> answer;
+
+    
+    switch (answer) {
+        case 20: legend = franko; break;
+        case 50: legend = grushevskiy; break;
+        case 100: legend = shevchenko; break;
+        case 200: legend = ukrainka; break;
+        case 500: legend = skovoroda; break;
+        case 1000: legend = vernadskiy; break;
+    }
+
+    cout << "На банкноті " << answer << " знаходиться ";
+
+    switch (legend) {
+        case franko:  cout << "Іван Франко" << endl; break;
+        case grushevskiy:  cout << "Михайло Грушевський" << endl; break;
+        case shevchenko:  cout << "Тарас Шевченко" << endl; break;
+        case ukrainka:  cout << "Леся Українка (Лариса Косач)" << endl; break;
+        case skovoroda:  cout << "Григорів Сковорода" << endl; break;
+        case vernadskiy:  cout << "Володимир Вернадський" << endl; break;
+    }
+
+
 }

dc55433b14d989770780acb00f7c5ae352edefac
kpei4ni <mkoragin09@gmail.com>
2024-04-07 17:27:49 +0300

    1 task with console out of symbols

--- Work/Work.cpp
+++ Work/Work.cpp
@@ -5,40 +5,21 @@
 #include <Windows.h>
 using namespace std;
 
-#define RED "\033[0;31m"
-#define GREEN "\033[0;32m"
-#define BLUE "\033[0;34m"
-#define YELLOW "\033[1;33m"
-#define CYAN "\033[0;36m"
-#define ORANGE "\033[0;33m"
-#define PURPLE "\033[0;35m"
-#define WHITE "\033[1;37m"
-#define GREY "\033[1;30m"
-#define PINK "\033[1;35m"
-#define BLACK "\033[0;30m"
-
-
-enum Bill {
-    
-};
-
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int number = 1;
-    int counter = 0;
+    string symbol;
+    int amount;
 
-    do {
-        cout << "¬вед≥ть число: "; cin >> number;
-        if (number == 0) break;
-        counter++;
-    } while (number != 0);
+    cout << "—имвол: "; cin >> symbol;
 
+    cout << "¬веди к≥льк≥сть символ≥в: "; cin >> amount;
 
-    cout << "Ѕуло введено " << counter << " „исел";
-
+    for (int i = 0; i < amount; i++) {
+        cout << symbol;
+    }
          
     return 0;
 }
